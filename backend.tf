071f48169aae409bb1f4110006c5ba7f4b0f8469
GargiAlmelkar <98380499+GargiAlmelkar@users.noreply.github.com>
2025-01-13 14:18:03 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-batch-26"
+    bucket = "gargialmelkar-terraformproject"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
